0ef36be6b0d029e97d8a94872d430f36a7771245
Minh Nguyễn <minhnguyen998vp@gmail.com>
2019-04-30 03:49:21 +0700

    minhnv push code fix web ok

--- WebBanSach/Models/EF/KhachHang.cs
+++ WebBanSach/Models/EF/KhachHang.cs
@@ -23,14 +23,14 @@ namespace WebBanSach.Models.EF
         public int KhachHangID { get; set; }
         public string TenKhachHang { get; set; }
         public Nullable<bool> GioiTinh { get; set; }
-        public Nullable<System.DateTime> NgaySInh { get; set; }
-        public string CMT { get; set; }
+        public Nullable<System.DateTime> NgaySinh { get; set; }
         public string DiaChi { get; set; }
         public string MatKhau_MaHoa { get; set; }
         public string TaiKhoan { get; set; }
         public Nullable<int> Tien { get; set; }
         public Nullable<int> Quyen { get; set; }
         public string SDT { get; set; }
+        public string Email { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<HoaDon> HoaDons { get; set; }
