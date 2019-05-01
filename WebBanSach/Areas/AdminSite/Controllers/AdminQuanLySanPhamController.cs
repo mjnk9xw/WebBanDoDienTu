4727ce9287f17175829a25e29fd02f9a7b4a4850
hoangminhdai880 <hoangminhdai880@gmail.com>
2019-05-01 17:14:52 +0700

    hoàn thành view admin

--- WebBanSach/Areas/AdminSite/Controllers/AdminQuanLySanPhamController.cs
+++ WebBanSach/Areas/AdminSite/Controllers/AdminQuanLySanPhamController.cs
@@ -64,70 +64,68 @@ namespace WebBanSach.Areas.AdminSite.Controllers
 
 
         }
-        //[HasCredential(Quyen = 1)]
-        //public ActionResult SuaSach(int MaSach)
-        //{
-        //    var TatCaChuDe = db.Chudes.ToList();
-        //    var TatCaTacGia = db.Tacgias.ToList();
-        //    var TatCaNXB = db.Nhaxuatbans.ToList();
-        //    ViewBag.TatCaChuDe = TatCaChuDe;
-        //    ViewBag.TatCaTacGia = TatCaTacGia;
-        //    ViewBag.TatCaNXB = TatCaNXB;
+        [HasCredential(Quyen = 1)]
+        public ActionResult SuaSanPham(int MaSach)
+        {
+            var TatCaTheLoai = db.TheLoais.ToList();
+            var TatCaNSX = db.NSXes.ToList();
+            ViewBag.TatCaTheLoai = TatCaTheLoai;
+            ViewBag.TatCaNXS = TatCaNSX;
 
-        //    //var sach = db.Saches.Join(db.Nhaxuatbans, s => s.Manxb, nxb => nxb.Manxb, (s, nxb) => s)
-        //    //                          .Join(db.Chudes, s => s.Macd, chude => chude.Macd, (s, chude) => s)
-        //    //                          .Join(db.Tacgias, s => s.Matacgia, tacgia => tacgia.Matacgia, (s, tacgia) => s)
-        //    //                          .Where(s => s.Masach == MaSach)
-        //    //                          .First();
-        //    Sach sach = db.Saches.Find(MaSach);
-        //    sach.Dongia = sach.Dongia != null ? sach.Dongia : 0;
-        //    sach.Giakm = sach.Giakm != null ? sach.Giakm : 0;
-        //    sach.Mota = sach.Mota != null ? sach.Mota : "không có mô tả";
-        //    sach.Motangangon = sach.Motangangon != null ? sach.Motangangon : "không có mô tả";
-        //    return View(sach);
-        //}
-        //[HttpPost]
-        //[HasCredential(Quyen = 1)]
-        //public ActionResult SuaSach(Sach sach, HttpPostedFileBase Hinhminhhoa)
-        //{
+            //var sach = db.Saches.Join(db.Nhaxuatbans, s => s.Manxb, nxb => nxb.Manxb, (s, nxb) => s)
+            //                          .Join(db.Chudes, s => s.Macd, chude => chude.Macd, (s, chude) => s)
+            //                          .Join(db.Tacgias, s => s.Matacgia, tacgia => tacgia.Matacgia, (s, tacgia) => s)
+            //                          .Where(s => s.Masach == MaSach)
+            //                          .First();
+            SanPham sanpham = db.SanPhams.Find(MaSach);
+            sanpham.Gia = sanpham.Gia != null ? sanpham.Gia : 0;
+            sanpham.GiaKM = sanpham.GiaKM != null ? sanpham.GiaKM : 0;
+            //sach.Mota = sach.Mota != null ? sach.Mota : "không có mô tả";
+            //sach.Motangangon = sach.Motangangon != null ? sach.Motangangon : "không có mô tả";
+            return View(sanpham);
+        }
+        [HttpPost]
+        [HasCredential(Quyen = 1)]
+        public ActionResult SuaSanPham(SanPham sanpham, HttpPostedFileBase Hinhminhhoa)
+        {
 
-        //    var a = sach;
-        //    if (Hinhminhhoa != null && Hinhminhhoa.ContentLength > 0)
-        //    {
-        //        var TenAnh = Path.GetFileName(Hinhminhhoa.FileName);
-        //        var DuongDan = Path.Combine(Server.MapPath("~/Assets/images/books/"), TenAnh);
-        //        sach.Hinhminhhoa = TenAnh;
-        //        Hinhminhhoa.SaveAs(DuongDan);
-        //    }
-        //    sach.Donvitinh = "VNĐ";
-        //    sach.Ngaycapnhat = DateTime.Now;
-        //    var sachcu = db.Saches.Find(sach.Masach);
-        //    db.Entry(sachcu).CurrentValues.SetValues(sach);
-        //    db.SaveChanges();
-        //    return RedirectToAction("TatCaSach");
+            var a = sanpham;
+            if (Hinhminhhoa != null && Hinhminhhoa.ContentLength > 0)
+            {
+                var TenAnh = Path.GetFileName(Hinhminhhoa.FileName);
+                var DuongDan = Path.Combine(Server.MapPath("~/Assets/images/"), TenAnh);
+                sanpham.AnhDaiDien = TenAnh;
+                Hinhminhhoa.SaveAs(DuongDan);
+            }
+            //sach.Donvitinh = "VNĐ";
+            sanpham.Ngay = DateTime.Now;
+            var sanphamcu = db.SanPhams.Find(sanpham.SanPhamID);
+            db.Entry(sanphamcu).CurrentValues.SetValues(sanpham);
+            db.SaveChanges();
+            return RedirectToAction("TatCaSanPham");
 
-        //}
-        //[HttpPost]
-        //[HasCredential(Quyen = 1)]
-        //public JsonResult XoaSach(int MaSach)
-        //{
-        //    try
-        //    {
-        //        Sach sach = db.Saches.Find(MaSach);
-        //        db.Saches.Remove(sach);
-        //        db.SaveChanges();
-        //        return Json(new { status = true });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return Json(new { status = false });
-        //    }
+        }
+        [HttpPost]
+        [HasCredential(Quyen = 1)]
+        public JsonResult XoaSanPham(int MaSanPham)
+        {
+            try
+            {
+                SanPham sanpham = db.SanPhams.Find(MaSanPham);
+                db.SanPhams.Remove(sanpham);
+                db.SaveChanges();
+                return Json(new { status = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = false });
+            }
 
-        //}
-        //public ActionResult TimKiem(string TenSach)
-        //{
-        //    List<Sach> lstSach = db.Saches.Where(x => x.Tensach.Contains(TenSach)).ToList();
-        //    return View("TatCaSach", lstSach);
-        //}
+        }
+        public ActionResult TimKiem(string TenSanPham)
+        {
+            List<SanPham> lstSanPham = db.SanPhams.Where(x => x.TenSanPham.Contains(TenSanPham)).ToList();
+            return View("TatCaSanPham", lstSanPham);
+        }
     }
 }
